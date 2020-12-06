3b1216cc24f008adadbf08f6272e547641cf9a56
ErwanDelory <erwan.delory@gmail.com>
2020-12-06 12:08:07 +0100

    add app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavbarLoto from './components/navbar';
+
+const App = () => {
+  return (
+    <div>
+      <NavbarLoto />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+const NavbarLoto = () => {
+  return (
+    <nav>
+      <Navbar bg="light">
+        <Navbar.Brand href="#home">Smart Loto</Navbar.Brand>
+      </Navbar>
+    </nav>
+  );
+};
+export default NavbarLoto;
